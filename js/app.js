a74bdfdff8a68b69e51321e85329521b842e4931
Sandra Velez <sandyvelez@gmail.com>
2017-10-05 21:07:33 -0400

    flipping logic done, reset done

--- js/app.js
+++ js/app.js
@@ -1,38 +1,9 @@
-let firstOpenCard;
 /*
- * Create a list that holds all of your cards
- */
-class Card {
-  constructor(icon) {
-    this.icon = icon;
-    // this.matched = false;
-    // this.flipped = false;
-  }
-}
-
-let deck = [new Card("fa fa-diamond"),
-  new Card("fa fa-diamond"),
-  new Card("fa fa-paper-plane-o"),
-  new Card("fa fa-paper-plane-o"),
-  new Card("fa fa-anchor"),
-  new Card("fa fa-anchor"),
-  new Card("fa fa-bolt"),
-  new Card("fa fa-bolt"),
-  new Card("fa fa-cube"),
-  new Card("fa fa-cube"),
-  new Card("fa fa-leaf"),
-  new Card("fa fa-leaf"),
-  new Card("fa fa-bicycle"),
-  new Card("fa fa-bicycle"),
-  new Card("fa fa-bomb"),
-  new Card("fa fa-bomb")
-];
-
-shuffle(deck);
-
-deal(deck);
-
-/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ *
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
@@ -42,79 +13,181 @@ deal(deck);
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-$('.card').click(function() {
 
-  let card = $(this);
 
-  openCard(card);
+// holds the first card selected for each attempt
+let firstOpenCard;
 
-  if (!save(card)) {
-    if (match(card)) {
-      console.log("They match!");
-    } else {
-      console.log("NO match!");
-      setTimeout(function() {
-        closeCards(card);
-      }, 1500);
+/*
+ * Card class
+ */
+class Card {
+    constructor(icon) {
+        this.icon = icon;
     }
-  }
+}
+
+// TODO, if you click twice on the same card, it shows up as a match
+
+let deck = createDeck();
+
+shuffle(deck);
+
+deal(deck);
+
+// bind card clicks to handler
+$('.card').bind("click", onEventClick);
+
+
+/*
+ * Event handler for clicks on cards
+ */
+function onEventClick(event) {
+    let card = $(this);
+
+    // flip the card over
+    openCard(card);
+
+    // save the first card selected
+    if (!save(card)) {
+        // if a card has already been saved, see if they match
+        if (match(card)) {
+            // if they match, lock them open
+            lockMatchedCards(card);
+        } else {
+            // if the cards don't match, wait a bit and then flip them closed
+            setTimeout(function() {
+                closeCards(card);
+            }, 750);
+        }
+    }
+};
+
+/*
+ * Handle the reset button
+ */
+$('.restart').click(function() {
+    $('.card').unbind("click", onEventClick);
+    $('.card').remove();
+    deck = createDeck();
+
+    shuffle(deck);
+    deal(deck);
+    $('.card').bind("click", onEventClick);
 });
 
-function save(card) {
-  let saved = false;
-
-  if (!firstOpenCard) {
-    firstOpenCard = card;
-    saved = true;
-  }
-
-  return saved;
-}
-
-function match(card) {
-  if (firstOpenCard.firstChild == card.firstChild) {
-    return true;
-  }
-}
-
-function openCard(card) {
-  card.attr("class", "card open show");
-}
-
-function closeCards(card) {
-  card.attr("class", "card");
-  firstOpenCard.attr("class", "card");
-  firstOpenCard = null;
+/*
+ * Create a deck of cards, 2 of each
+ */
+function createDeck() {
+    return selectedDeck = [
+        new Card("fa fa-diamond"),
+        new Card("fa fa-diamond"),
+        new Card("fa fa-paper-plane-o"),
+        new Card("fa fa-paper-plane-o"),
+        new Card("fa fa-anchor"),
+        new Card("fa fa-anchor"),
+        new Card("fa fa-bolt"),
+        new Card("fa fa-bolt"),
+        new Card("fa fa-cube"),
+        new Card("fa fa-cube"),
+        new Card("fa fa-leaf"),
+        new Card("fa fa-leaf"),
+        new Card("fa fa-bicycle"),
+        new Card("fa fa-bicycle"),
+        new Card("fa fa-bomb"),
+        new Card("fa fa-bomb")
+    ];
 }
 
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
+ * save the first selected card
  */
+function save(card) {
 
-// deal the cards
-function deal(deck) {
-  for (let x = 0; x < deck.length; x++) {
-    let newCard = "<i class='" + deck[x].icon + "'>";
-    let dealtCard = $('<li class="card">' + newCard + '</i></li>');
-    $('.deck').append(dealtCard);
-  }
+    let saved = false;
+
+    // only save the first selected card, no need to save the second one
+    if (!firstOpenCard) {
+        firstOpenCard = card;
+        saved = true;
+    }
+
+    return saved;
 }
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+/*
+ *  check to see if the cards match
+ */
+function match(card) {
+    let card1 = firstOpenCard.find('i');
+    let card2 = card.find('i');
+
+    if (card1[0].className === card2[0].className) {
+        return true;
+    }
+}
+
+/*
+ * display a clicked card
+ */
+function openCard(card) {
+    card.attr("class", "card open show");
+}
+
+/*
+ * close the opened cards
+ */
+function closeCards(card) {
+    card.attr("class", "card");
+    firstOpenCard.attr("class", "card");
+
+    deselectCards();
+}
+
+/*
+ *  cards matched, lock them open
+ */
+function lockMatchedCards(card) {
+    card.attr("class", "card match");
+    firstOpenCard.attr("class", "card match");
+
+    deselectCards();
+}
+
+/*
+ *  clear selected cards
+ */
+function deselectCards() {
+    card = undefined;
+    firstOpenCard = undefined;
+}
+
+/*
+ *  deal the cards
+ */
+function deal(deck) {
+    for (let x = 0; x < deck.length; x++) {
+        let newCard = "<i class='" + deck[x].icon + "'>";
+        let dealtCard = $('<li class="card">' + newCard + '</i></li>');
+        $('.deck').append(dealtCard);
+    }
+}
+
+/*
+ *  Shuffle function from http://stackoverflow.com/a/2450976
+ */
 function shuffle(array) {
-  var currentIndex = array.length,
-    temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
-  while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
 
-  return array;
+    return array;
 }
